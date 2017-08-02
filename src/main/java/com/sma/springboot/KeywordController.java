68632bb317f18e0fbdd1ef85148e5dcc12af4815
Seonghun Kang <neuron03@gmail.com>
2017-08-02 14:54:08 +0900

    initial commit

---
+++ src/main/java/com/sma/springboot/KeywordController.java
@@ -0,0 +1,12 @@
+package com.sma.springboot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KeywordController {
+	
+	
+
+}
---
+++ src/main/java/com/sma/springboot/KeywordServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sma.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordServiceApplication.class, args);
+	}
+}
